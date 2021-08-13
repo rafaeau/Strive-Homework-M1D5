aec5adb5e94be81d33a7ba4444ff464dd643e785
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-08-13 18:23:59 +0200

    Question 5 solved

--- d5.js
+++ d5.js
@@ -72,7 +72,19 @@ Write a function "strivify" which accepts a string.
 It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-/* WRITE YOUR CODE HERE */
+const strivify = function (text){
+    if (text === text.startsWith("Strive")){
+        return text
+    }
+    else {
+        return "Strive " + text
+    }
+}
+text = "School is a tech school based in Berlin."
+console.log(strivify(text))
+
+console.log(strivify ("School is a tech school based in Berlin."))
+
 
 /* EXERCISE 6
 Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
@@ -85,7 +97,24 @@ HINT: Module Operator
 Write a function "reverseString" to programmatically reverse a given string (es.: Strive => evirtS).
 */
 
-/* WRITE YOUR CODE HERE */
+/* function reverseString(str) {
+    return str;
+}
+const nameFunc = strive
+const splitNameFunc = nameFunc.split(' ')
+
+const splitNameFunc = ['s', 't', 'r', 'i', 'v', 'e']
+const reverseNameFunc = splitNameFunc.reverse()
+
+const reverseNameFunc = ['e', 'v', 'i', 'r', 't', 's']
+const result = reverseNameFunc.join(' ')
+
+function reverseString(str) {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+} */
 
 /* EXERCISE 8
 Write a function "upperFirst" to capitalize the first letter of each word of a given string passed as a parameter.
